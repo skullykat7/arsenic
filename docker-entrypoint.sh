607f977df464fe35eeac141352abaac2793b170c
skullykat7 <roman.karasev@tvsnaples.org>
2026-09-23 14:05:19 -0400

    Modify nginx entrypoint for dynamic PORT and non-root user
    Updated nginx configuration to listen on dynamic PORT and modified entrypoint script for better compatibility with non-root users.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,14 +4,16 @@ set -e
 
 # environment variables are set at runtime, so modify the conf only when it starts
 
-
 if [ ! -z "$SAFE_BROWSING" ]; then
     sed -i "s/1.1.1.1/1.1.1.3/" /etc/nginx/nginx.conf
 fi
 
+# Make nginx listen on the PORT that Hostless (or Heroku) gives us
+sed -i "s/listen 80;/listen ${PORT:-80};/" /etc/nginx/nginx.conf
 
 # default.conf makes docker listen to 80, and
-# heroku's non-root user running this won't like it at all
-rm /etc/nginx/conf.d/default.conf
+# non-root users won't like it
+rm -f /etc/nginx/conf.d/default.conf
 
-nginx
+# daemon off; was already injected by docker-sed.sh at build time
+exec nginx
