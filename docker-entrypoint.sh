094a0ea29347408005756b867729ffae1ecd8b74
skullykat7 <roman.karasev@tvsnaples.org>
2026-09-23 13:57:38 -0400

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 # environment variables are set at runtime, so modify the conf only when it starts
-sed -i "s/listen 80/listen $PORT/" /etc/nginx/nginx.conf
+
 
 if [ ! -z "$SAFE_BROWSING" ]; then
     sed -i "s/1.1.1.1/1.1.1.3/" /etc/nginx/nginx.conf
