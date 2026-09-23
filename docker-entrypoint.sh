ccac039ee54911f98827c9393ecf70dd00cd2e0f
skullykat7 <roman.karasev@tvsnaples.org>
2026-09-23 14:11:57 -0400

    Refactor docker-entrypoint.sh for nginx configuration

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 set -e
 
-# Optional Safe Browsing (only if the env var is set)
+# Optional Safe Browsing
 if [ -n "$SAFE_BROWSING" ]; then
   sed -i "s/1.1.1.1/1.1.1.3/" /etc/nginx/nginx.conf
 fi
 
-# Remove the default config that the official nginx image ships
-rm -f /etc/nginx/conf.d/default.conf
-
-# Substitute ${PORT} and start nginx
-envsubst '${PORT}' < /etc/nginx/nginx.conf > /tmp/nginx.conf
-mv /tmp/nginx.conf /etc/nginx/nginx.conf
+# Substitute the PORT variable and start nginx
+envsubst '${PORT}' < /etc/nginx/nginx.conf > /tmp/nginx.conf \
+  && cat /tmp/nginx.conf > /etc/nginx/nginx.conf
 
 exec nginx
